528f4c4c3af075e298f2bb8205be03ba09d9d1e4
abhinavtyagi <abhi007tyagi@yahoo.co.in>
2017-02-03 10:30:04 +0530

    Initial commit with template

---
+++ app/src/main/java/com/tyagiabhinav/mvprxapp/ui/MainContract.java
@@ -0,0 +1,16 @@
+package com.tyagiabhinav.mvprxapp.ui;
+
+/**
+ * Created by abhinavtyagi on 03/02/17.
+ */
+
+public interface MainContract {
+
+    interface View{
+
+    }
+
+    interface Presenter{
+
+    }
+}
---
+++ app/src/main/java/com/tyagiabhinav/mvprxapp/ui/MainPresenter.java
@@ -0,0 +1,8 @@
+package com.tyagiabhinav.mvprxapp.ui;
+
+/**
+ * Created by abhinavtyagi on 03/02/17.
+ */
+
+public class MainPresenter implements MainContract.Presenter {
+}
